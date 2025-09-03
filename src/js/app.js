62f571314ad769b3c120aa8df849ea0227ef0581
kseniya-st <kseniya.stoichykava@alreadymedia.com>
2025-09-03 16:19:34 +0300

    add modal

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,4 @@ import { initForm } from "./fragments/form";
 import { initButton } from "./fragments/button";
 import { imgGallery } from "./fragments/imgGallery";
 import { initLang } from "./fragments/lang";
+import { initModal } from "./blocks/modal";
