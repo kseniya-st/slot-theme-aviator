2b8520b285b220d0fcd76a12e436451fa1499959
kseniya-st <kseniya.stoichykava@alreadymedia.com>
2025-09-03 14:28:57 +0300

    remove unused components

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
 import { header } from "./blocks/header";
-import { initToc } from "./blocks/toc";
-import { initAccord } from "./fragments/accord";
 import { initScrollContainer } from "./fragments/scrollContainer";
 import { initForm } from "./fragments/form";
-import { initCopyBtn } from "./fragments/copyBtn";
 import { initButton } from "./fragments/button";
 import { imgGallery } from "./fragments/imgGallery";
+import { initLang } from "./fragments/lang";
